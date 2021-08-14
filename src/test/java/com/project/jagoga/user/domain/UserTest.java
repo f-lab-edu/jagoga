25cad25070c747e81c02510f09a9d61bab4ab163
baesuyeon <37732016+baesuyeon@users.noreply.github.com>
2021-08-15 04:24:05 +0900

    test: 이름, 비밀번호, 전화번호 체크

--- src/test/java/com/project/jagoga/user/domain/UserTest.java
+++ src/test/java/com/project/jagoga/user/domain/UserTest.java
@@ -46,6 +46,7 @@ class UserTest {
         @Nested
         @DisplayName("실패 케이스")
         class createUser_FailCases {
+
             @Nested
             @DisplayName("이메일")
             class userId {
@@ -92,6 +93,156 @@ class UserTest {
 
                     // then
                     assertEquals("이메일이 존재하지 않습니다", exception.getMessage());
+                }
+            }
+
+            @Nested
+            @DisplayName("이름")
+            class userName {
+
+                @Test
+                @DisplayName("null")
+                void failNullName() {
+                    // given
+                    name = null;
+
+                    UserCreateRequestDto userCreateRequestDto = new UserCreateRequestDto(email, name, password, phone);
+
+                    // when
+                    Exception exception = assertThrows(Exception.class, userCreateRequestDto::toEntity);
+
+                    // then
+                    assertEquals("이름이 존재하지 않습니다", exception.getMessage());
+                }
+
+                @Test
+                @DisplayName("빈 문자열")
+                void failEmptyName() {
+                    // given
+                    name = "";
+
+                    UserCreateRequestDto userCreateRequestDto = new UserCreateRequestDto(email, name, password, phone);
+
+                    // when
+                    Exception exception = assertThrows(Exception.class, userCreateRequestDto::toEntity);
+
+                    // then
+                    assertEquals("이름이 존재하지 않습니다", exception.getMessage());
+                }
+
+                @Test
+                @DisplayName("공백 문자열")
+                void failBlankName() {
+                    // given
+                    name = "  ";
+
+                    UserCreateRequestDto userCreateRequestDto = new UserCreateRequestDto(email, name, password, phone);
+
+                    // when
+                    Exception exception = assertThrows(Exception.class, userCreateRequestDto::toEntity);
+
+                    // then
+                    assertEquals("이름이 존재하지 않습니다", exception.getMessage());
+                }
+            }
+
+            @Nested
+            @DisplayName("비밀번호")
+            class userPassword {
+
+                @Test
+                @DisplayName("null")
+                void failNullPassword() {
+                    // given
+                    password = null;
+
+                    UserCreateRequestDto userCreateRequestDto = new UserCreateRequestDto(email, name, password, phone);
+
+                    // when
+                    Exception exception = assertThrows(Exception.class, userCreateRequestDto::toEntity);
+
+                    // then
+                    assertEquals("비밀번호가 존재하지 않습니다", exception.getMessage());
+                }
+
+                @Test
+                @DisplayName("빈 문자열")
+                void failEmptyPassword() {
+                    // given
+                    password = "";
+
+                    UserCreateRequestDto userCreateRequestDto = new UserCreateRequestDto(email, name, password, phone);
+
+                    // when
+                    Exception exception = assertThrows(Exception.class, userCreateRequestDto::toEntity);
+
+                    // then
+                    assertEquals("비밀번호가 존재하지 않습니다", exception.getMessage());
+                }
+
+                @Test
+                @DisplayName("공백 문자열")
+                void failBlankPassword() {
+                    // given
+                    password = "  ";
+
+                    UserCreateRequestDto userCreateRequestDto = new UserCreateRequestDto(email, name, password, phone);
+
+                    // when
+                    Exception exception = assertThrows(Exception.class, userCreateRequestDto::toEntity);
+
+                    // then
+                    assertEquals("비밀번호가 존재하지 않습니다", exception.getMessage());
+                }
+            }
+
+            @Nested
+            @DisplayName("전화번호")
+            class userPhone {
+
+                @Test
+                @DisplayName("null")
+                void failNullPhone() {
+                    // given
+                    phone = null;
+
+                    UserCreateRequestDto userCreateRequestDto = new UserCreateRequestDto(email, name, password, phone);
+
+                    // when
+                    Exception exception = assertThrows(Exception.class, userCreateRequestDto::toEntity);
+
+                    // then
+                    assertEquals("전화번호가 존재하지 않습니다", exception.getMessage());
+                }
+
+                @Test
+                @DisplayName("빈 문자열")
+                void failEmptyPhone() {
+                    // given
+                    phone = "";
+
+                    UserCreateRequestDto userCreateRequestDto = new UserCreateRequestDto(email, name, password, phone);
+
+                    // when
+                    Exception exception = assertThrows(Exception.class, userCreateRequestDto::toEntity);
+
+                    // then
+                    assertEquals("전화번호가 존재하지 않습니다", exception.getMessage());
+                }
+
+                @Test
+                @DisplayName("공백 문자열")
+                void failBlankPhone() {
+                    // given
+                    phone = "  ";
+
+                    UserCreateRequestDto userCreateRequestDto = new UserCreateRequestDto(email, name, password, phone);
+
+                    // when
+                    Exception exception = assertThrows(Exception.class, userCreateRequestDto::toEntity);
+
+                    // then
+                    assertEquals("전화번호가 존재하지 않습니다", exception.getMessage());
                 }
             }
         }
