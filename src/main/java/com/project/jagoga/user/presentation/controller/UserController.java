48bf631cd8dbbd5609c6123fcaa33e48c90925da
baesuyeon <37732016+baesuyeon@users.noreply.github.com>
2021-08-17 13:15:56 +0900

    refactor: URI 포맷 수정

--- src/main/java/com/project/jagoga/user/presentation/controller/UserController.java
+++ src/main/java/com/project/jagoga/user/presentation/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api/users")
 public class UserController {
 
     private final UserService userService;
