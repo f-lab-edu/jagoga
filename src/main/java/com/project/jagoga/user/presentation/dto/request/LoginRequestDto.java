67722d1b1d34bb1df27c2343a1a8568ac47dd470
baesuyeon <37732016+baesuyeon@users.noreply.github.com>
2021-08-21 20:39:55 +0900

    add: dto에 유효성 검증 어노테이션 추가

--- src/main/java/com/project/jagoga/user/presentation/dto/request/LoginRequestDto.java
+++ src/main/java/com/project/jagoga/user/presentation/dto/request/LoginRequestDto.java
@@ -1,11 +1,17 @@
 package com.project.jagoga.user.presentation.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
+@AllArgsConstructor
 public class LoginRequestDto {
 
-    // TODO : validation
+    @NotBlank(message = "이메일은 빈 값일 수 없습니다")
     private String email;
+
+    @NotBlank(message = "비밀번호는 빈 값일 수 없습니다")
     private String password;
 }
