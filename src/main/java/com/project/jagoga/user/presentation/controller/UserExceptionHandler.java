cd71499f8f98e2e34457f0d9970bb11a210d68fa
baesuyeon <37732016+baesuyeon@users.noreply.github.com>
2021-08-21 20:06:56 +0900

    add: UserAuthenticationFailException 발생시 처리

--- src/main/java/com/project/jagoga/user/presentation/controller/UserExceptionHandler.java
+++ src/main/java/com/project/jagoga/user/presentation/controller/UserExceptionHandler.java
@@ -3,6 +3,7 @@ package com.project.jagoga.user.presentation.controller;
 import com.project.jagoga.exception.dto.ApiResponse;
 import com.project.jagoga.exception.user.DuplicatedUserException;
 import com.project.jagoga.exception.user.NotFoundUserException;
+import com.project.jagoga.exception.user.UserAuthenticationFailException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,6 +20,11 @@ public class UserExceptionHandler {
     // TODO : Json 응답 포맷 통일하기
     @ExceptionHandler(NotFoundUserException.class)
     public ResponseEntity<String> handleNotFoundUserException(RuntimeException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(UserAuthenticationFailException.class)
+    public ResponseEntity<String> handleUserAuthenticationFailException(RuntimeException exception) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
     }
 }
