79554212ad3ef32d67813468d76447b2195739b5
baesuyeon <37732016+baesuyeon@users.noreply.github.com>
2021-08-22 16:52:21 +0900

    test: 로그인 테스트 생성

---
+++ src/test/java/com/project/jagoga/user/presentation/controller/UserControllerTest.java
@@ -0,0 +1,68 @@
+package com.project.jagoga.user.presentation.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.jagoga.user.application.impl.UserServiceImpl;
+import com.project.jagoga.user.domain.User;
+import com.project.jagoga.user.presentation.dto.request.LoginRequestDto;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    UserServiceImpl userService;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    User user;
+    String email;
+    String name;
+    String phone;
+    String password;
+
+    @BeforeEach
+    public void setUp() {
+        email = "test1223@test";
+        name = "testname";
+        password = "testpassword";
+        phone = "testphone";
+        user = User.createInstance(email, name, password, phone);
+    }
+
+    @Test
+    @DisplayName("사용자 로그인 테스트")
+    public void login() throws Exception {
+        // given
+        userService.signUp(user);
+
+        LoginRequestDto loginRequestDto = new LoginRequestDto(email, password);
+        String loginJson = objectMapper.writeValueAsString(loginRequestDto);
+
+        // when then
+        mockMvc.perform(post("/api/users/login").contentType(MediaType.APPLICATION_JSON)
+                .content(loginJson))
+                .andExpect(status().isOk())
+                .andExpect((ResultMatcher) content().string(Matchers.containsString("accessToken")));
+    }
+}
