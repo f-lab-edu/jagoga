fc67f81ef13fa533d484c8eef93a5c6206a85da3
baesuyeon <37732016+baesuyeon@users.noreply.github.com>
2021-08-23 17:47:37 +0900

    refactor: 테스트시 application-dev.properties 사용하도록 수정

---
+++ src/main/java/com/project/jagoga/global/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.project.jagoga.global.config;
+
+import com.project.jagoga.user.presentation.interceptor.AuthenticationInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthenticationInterceptor authenticationInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authenticationInterceptor);
+    }
+}
---
+++ src/main/java/com/project/jagoga/user/presentation/interceptor/AuthenticationInterceptor.java
@@ -0,0 +1,30 @@
+package com.project.jagoga.user.presentation.interceptor;
+
+import com.project.jagoga.user.domain.Authentication;
+import com.project.jagoga.user.domain.LoginCheck;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+@RequiredArgsConstructor
+public class AuthenticationInterceptor implements HandlerInterceptor {
+
+    private final Authentication authentication;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (((HandlerMethod) handler).getMethodAnnotation(LoginCheck.class) == null) {
+            return true;
+        }
+
+        String token = request.getHeader(HttpHeaders.AUTHORIZATION);
+        authentication.verifyLogin(token);
+        return true;
+    }
+}
