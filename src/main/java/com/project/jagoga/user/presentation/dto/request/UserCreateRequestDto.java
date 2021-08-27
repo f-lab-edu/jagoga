95ddb7851c5afbc8eb59cf098069b282b3309495
baesuyeon <37732016+baesuyeon@users.noreply.github.com>
2021-08-28 02:16:20 +0900

    refactor: 비밀번호 유효성 추가

--- src/main/java/com/project/jagoga/user/presentation/dto/request/UserCreateRequestDto.java
+++ src/main/java/com/project/jagoga/user/presentation/dto/request/UserCreateRequestDto.java
@@ -4,6 +4,7 @@ import com.project.jagoga.user.domain.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.hibernate.validator.constraints.Length;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
@@ -22,7 +23,8 @@ public class UserCreateRequestDto {
     private String name;
 
     @NotBlank(message = "비밀번호는 빈 값일 수 없습니다")
-    @Length(max = 20, message = "비밀번호는 20자 이내로 입력하세요")
+    @Pattern(regexp = "(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,20}",
+            message = "패스워드는 대문자, 소문자, 특수문자가 적어도 하나씩은 있어야 하며 최소 8자리, 최대 20자리까지 가능합니다")
     private String password;
 
     @NotBlank(message = "전화번호는 빈 값일 수 없습니다")
--- src/main/java/com/project/jagoga/user/presentation/dto/request/UserUpdateRequestDto.java
+++ src/main/java/com/project/jagoga/user/presentation/dto/request/UserUpdateRequestDto.java
@@ -16,7 +16,8 @@ public class UserUpdateRequestDto {
     private String name;
 
     @NotBlank(message = "비밀번호는 빈 값일 수 없습니다")
-    @Length(max = 20, message = "비밀번호는 20자 이내로 입력하세요")
+    @Pattern(regexp = "(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,20}",
+            message = "패스워드는 대문자, 소문자, 특수문자가 적어도 하나씩은 있어야 하며 최소 8자리, 최대 20자리까지 가능합니다")
     private String password;
 
     @NotBlank(message = "전화번호는 빈 값일 수 없습니다")
