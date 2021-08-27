379adbcdde82b3e0a4ba965874f34f5b18fef02d
baesuyeon <37732016+baesuyeon@users.noreply.github.com>
2021-08-27 14:07:13 +0900

    refactor: LoginRequestDto에 Email validator 추가

--- src/main/java/com/project/jagoga/user/presentation/dto/request/LoginRequestDto.java
+++ src/main/java/com/project/jagoga/user/presentation/dto/request/LoginRequestDto.java
@@ -3,6 +3,7 @@ package com.project.jagoga.user.presentation.dto.request;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @Getter
@@ -10,6 +11,7 @@ import javax.validation.constraints.NotBlank;
 public class LoginRequestDto {
 
     @NotBlank(message = "이메일은 빈 값일 수 없습니다")
+    @Email(message = "올바른 형식의 이메일 주소어야 합니다")
     private String email;
 
     @NotBlank(message = "비밀번호는 빈 값일 수 없습니다")
