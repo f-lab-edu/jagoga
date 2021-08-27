bac8dca3751893d7b8fce8142484220676fececa
baesuyeon <37732016+baesuyeon@users.noreply.github.com>
2021-08-27 16:41:23 +0900

    refactor: Login 한 사용자에 대해서만 사용자 변경할 수 있도록 수정

--- src/main/java/com/project/jagoga/user/presentation/controller/UserController.java
+++ src/main/java/com/project/jagoga/user/presentation/controller/UserController.java
@@ -3,6 +3,7 @@ package com.project.jagoga.user.presentation.controller;
 import com.project.jagoga.exception.dto.ApiResponse;
 import com.project.jagoga.user.application.UserService;
 import com.project.jagoga.user.domain.Authentication;
+import com.project.jagoga.user.domain.LoginCheck;
 import com.project.jagoga.user.domain.User;
 import com.project.jagoga.user.presentation.dto.request.LoginRequestDto;
 import com.project.jagoga.user.presentation.dto.request.UserCreateRequestDto;
@@ -36,6 +37,7 @@ public class UserController {
         return ApiResponse.createSuccess(JwtResponseDto.createInstance(token));
     }
 
+    @LoginCheck
     @PutMapping("/{id}")
     public ApiResponse<UserResponseDto> updateUser(@PathVariable("id") final long id, @Valid @RequestBody final UserUpdateRequestDto userUpdateRequestDto) {
         User user = userService.updateUser(id, userUpdateRequestDto);
