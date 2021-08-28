75a10ceb37690877190e6274c4e7fd63848d5de9
baesuyeon <37732016+baesuyeon@users.noreply.github.com>
2021-08-28 16:59:21 +0900

    feat: Role 권한 검사를 위한 어노테이션 생성

--- src/main/java/com/project/jagoga/user/domain/RequireLoginUser.java
+++ src/main/java/com/project/jagoga/user/domain/RequireLoginUser.java
@@ -1,4 +1,15 @@
 package com.project.jagoga.user.domain;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/*
+    해당 어노테이션을 컨트롤러 메서드 파라미터에 사용하면
+    컨트롤러 메서드가 호출되기 전, 인자값으로 AuthUser(로그인한 유저의 정보)를 전달합니다.
+ */
+@Target(value = {ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface RequireLoginUser {
 }
