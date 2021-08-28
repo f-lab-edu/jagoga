1ecec1b7e29892febf3d731b54ba7704a367e340
baesuyeon <37732016+baesuyeon@users.noreply.github.com>
2021-08-28 15:00:59 +0900

    feat: User에 role 필드 추가 및 생성자 변경

--- src/main/java/com/project/jagoga/user/domain/User.java
+++ src/main/java/com/project/jagoga/user/domain/User.java
@@ -13,6 +13,7 @@ public class User {
     private String name;
     private String password;
     private String phone;
+    private Role role;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
@@ -25,7 +26,7 @@ public class User {
     }
 
     public User updateUser(String name, String password, String phone) {
-        return new User(this.id, this.email, name, password, phone);
+        return new User(this.id, this.email, name, password, phone, this.role);
     }
 
     public static User createInstance(String email, String name, String password, String phone) {
@@ -34,21 +35,27 @@ public class User {
         Assert.hasText(password, "비밀번호가 존재하지 않습니다");
         Assert.hasText(phone, "전화번호가 존재하지 않습니다");
 
-        return new User(email, name, password, phone);
+        return new User(email, name, password, phone, getUserDefaultRole());
     }
 
-    private User(String email, String name, String password, String phone) {
+    private static Role getUserDefaultRole() {
+        return Role.BASIC;
+    }
+
+    private User(String email, String name, String password, String phone, Role role) {
         this.email = email;
         this.name = name;
         this.password = password;
         this.phone = phone;
+        this.role = role;
     }
 
-    private User(long id, String email, String name, String password, String phone) {
+    private User(long id, String email, String name, String password, String phone, Role role) {
         this.id = id;
         this.email = email;
         this.name = name;
         this.password = password;
         this.phone = phone;
+        this.role = role;
     }
 }
