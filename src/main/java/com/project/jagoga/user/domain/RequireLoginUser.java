542bfc3126af020f2d05258e0fe2b4017128a626
baesuyeon <37732016+baesuyeon@users.noreply.github.com>
2021-08-28 16:58:22 +0900

    style: 어노테이션 설명 추가

---
+++ src/main/java/com/project/jagoga/user/domain/RequireLoginUser.java
@@ -0,0 +1,4 @@
+package com.project.jagoga.user.domain;
+
+public @interface RequireLoginUser {
+}
---
+++ src/main/java/com/project/jagoga/user/presentation/resolver/RequireLoginUserArgumentResolver.java
@@ -0,0 +1,4 @@
+package com.project.jagoga.user.presentation.resolver;
+
+public class RequireLoginUserArgumentResolver {
+}
