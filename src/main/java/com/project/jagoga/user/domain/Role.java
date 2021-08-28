55fa41280657ec138bd1ac95a70410518938f7c5
baesuyeon <37732016+baesuyeon@users.noreply.github.com>
2021-08-28 15:00:37 +0900

    feat: Role enum 클래스 추가

--- src/main/java/com/project/jagoga/user/domain/Role.java
+++ src/main/java/com/project/jagoga/user/domain/Role.java
@@ -1,4 +1,16 @@
 package com.project.jagoga.user.domain;
 
 public enum Role {
+
+    BASIC("일반 사용자"), OWNER("사장님"), ADMIN("관리자");
+
+    private final String value;
+
+    Role(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
