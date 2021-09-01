da8290decef3011f5cc096092f9fa015a302fe61
oopsys6 <operasystem6@naver.com>
2021-09-01 16:09:15 +0900

    refactor: Util 클래스 private 생성자 추가

--- src/main/java/com/project/jagoga/utils/VerificationUtils.java
+++ src/main/java/com/project/jagoga/utils/VerificationUtils.java
@@ -3,7 +3,10 @@ package com.project.jagoga.utils;
 import com.project.jagoga.exception.user.ForbiddenException;
 import com.project.jagoga.user.domain.AuthUser;
 import com.project.jagoga.user.domain.Role;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class VerificationUtils {
 
     public static void verifyPermission(AuthUser loginUser, long userId) {
