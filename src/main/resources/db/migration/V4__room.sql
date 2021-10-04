eceb7b9bd5c15ea65b6c816ec421f22a7015b1e0
baesuyeon <37732016+baesuyeon@users.noreply.github.com>
2021-10-04 15:03:31 +0900

    delete: currentBookingId, RoomStatus DB 컬럼 제거

--- src/main/resources/db/migration/V4__room.sql
+++ src/main/resources/db/migration/V4__room.sql
@@ -2,8 +2,6 @@ create table room (
     room_id BIGINT AUTO_INCREMENT PRIMARY KEY,
     roomtype_id BIGINT not null,
     room_number varchar(50) not null,
-    current_booking_id BIGINT,
-    room_status varchar(50),
     created_at DATETIME,
     modified_at DATETIME,
     FOREIGN KEY (roomtype_id)
